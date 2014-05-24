3fc805a2ee6d70a5cdf5f69fc25e248b8913c7ce
Yigit Guler <yigit@hipo.biz>
2014-05-24 13:39:35 +0300

    Initial commit! YAY

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'H. Yi\u011fit G\xfcler'
+SITENAME = u'Portu\xe7'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'tr'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (
+    ('Hipo', 'http://hipolabs.com/'),
+)
+
+# Social widget
+SOCIAL = (
+    ('Twitter', 'https://twitter.com/HYigitGuler/'),
+)
+
+DEFAULT_PAGINATION = 5
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
