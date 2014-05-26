866a279d24b6107c54963b742cda8e14cf712675
Yigit Guler <yigit@hipo.biz>
2014-05-27 01:40:49 +0300

    Added custom theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -36,3 +36,4 @@ EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
 GOOGLE_ANALYTICS = "UA-51361525-1"
 TWITTER_USERNAME = "hyigitguler"
 DISQUS_SITENAME = "portuc"
+THEME = "themes/itsmyidea/"
